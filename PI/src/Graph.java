f5fbe10856c797c3572a9d7bab03a058be2c5fe6
Remi ROBIN <remi.robin@polytechnique.edu>
2016-12-06 15:31:40 +0100

    complete isvalidcoloring method

--- PI/src/Graph.java
+++ PI/src/Graph.java
@@ -1,12 +1,13 @@
-import java.util.Queue;
+import java.util.LinkedList;
+import java.util.ListIterator;
 
 public class Graph {
 	int number; // reference of the graph
-	int n; // number of nodes
+	int n; // number of nodes - K
 	int K;
-	Queue<Queue<Integer>> edges;
+	LinkedList<Integer>[] edges;
 	int[] hint;
-	public void Graph(int number, int n, int K, Queue<Queue<Integer>> edges, int[] hint){
+	public void Graph(int number, int n, int K, LinkedList<Integer>[] edges, int[] hint){
 		this.number=number;
 		this.n=n;
 		this.K=K;
@@ -21,7 +22,14 @@ public class Graph {
 				}
 			}
 		}
-		//if we arrive this point, it means that the K first nodes are colored with differents colors
-		//while
+		for (int i=0; i< n -this.K;i++){
+			ListIterator li = edges[i].listIterator();
+			int c=colors[i+K];
+			while (li.hasNext()){
+				if (colors[li.next()]==c) return false;
+			}
+			
+		}
+	return true;
 	}
 }
