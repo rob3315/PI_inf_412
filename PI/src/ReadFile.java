f299fb4dba7f7b0d75f108d647d321d9146b48ab
Lorraine ROULIER <lorraine.roulier@polytechnique.edu>
2016-12-06 15:25:15 -0800

    new commit

--- PI/src/ReadFile.java
+++ PI/src/ReadFile.java
@@ -2,9 +2,9 @@ import java.io.*;
 import java.util.*;
 
 public class ReadFile {
-	public static void main(String[] args) {
+	//public static void main(String[] args) {
+	public static LinkedList<Graph> datasetReader(String fichier){
 		String graphs = "";
-		String fichier = "GraphTest.txt";
 
 		// lecture du fichier texte
 		try {
@@ -32,6 +32,7 @@ public class ReadFile {
 		String[] G = graphs.split(":");
 
 		int n = G.length;
+		LinkedList<Graph> graphes = new LinkedList<Graph>();
 		String graphID = "";
 		String colored = "";
 		String nbvertices = "";
@@ -39,7 +40,9 @@ public class ReadFile {
 		// String adjacency="";
 		for (int i = 0; i < n; i++) {
 			if (G[i].length() > 0) {
+				Graph g= new Graph();
 				if (G[i].charAt(0) == 'G')
+					G[i].split(" ");
 					graphID += G[i];
 				// else{
 				if (G[i].charAt(0) == 'K')
@@ -56,6 +59,7 @@ public class ReadFile {
 		// for (int i =0; i<ID.length; i++)
 		// System.out.println(ID[i]);
 		int nbGraph = ID.length;
+		return graphes;
 
 	}
 
--- PI/src/valid_coloring.java
+++ PI/src/valid_coloring.java
@@ -1,4 +1,8 @@
 
 public class valid_coloring {
-
+	public static void main(String[] args) {
+		String fichier="datasetA.txt";
+		ReadFile.datasetReader(fichier);
+		
+	}
 }
